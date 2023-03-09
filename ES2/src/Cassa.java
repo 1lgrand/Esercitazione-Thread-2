b94fd82f2c8867d82cf00815b6286b60e86d4dd9
Alessandro De Marzo <demarzoad@libero.it>
2023-03-09 17:17:42 +0100

    Aggiunto es2, non completato

---
+++ ES2/src/Cassa.java
@@ -0,0 +1,15 @@
+public class Cassa implements Runnable{
+
+    private static int biglietti = 200;
+    private String nomeCassa;
+
+    public Cassa(String nomeCassa) {
+        this.nomeCassa = nomeCassa;
+    }
+
+
+    @Override
+    public void run(){
+
+    }
+}
